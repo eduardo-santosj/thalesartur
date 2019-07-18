69b60eea2887ecaab616e471c81423acf455f14b
Eduardo Santos <eduardosantosj2@gmail.com>
2019-07-17 23:22:50 -0300

    Deploy umbler

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -20,8 +20,8 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'contato@scalte.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contato do site:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$email_body = "Você recebeu uma nova mensagem no seu site.\n\n"."Aqui vem os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nTelefone: $phone\n\nassunto:$assunto\n\nMessage:\n$message";
+$headers = "From: $email_address\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
